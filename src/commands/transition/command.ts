6174cbf919c762b10480373fd36a40cf4bb65cc4
Mathieu Bour <mat@fantium.com>
2026-01-28 10:49:48 +0100

    feat: update launch and transition commands to include streamflow option as default

--- src/commands/transition/command.ts
+++ src/commands/transition/command.ts
@@ -25,7 +25,8 @@ const transitionCommand = new Command('transition')
 	)
 	.option('-s, --send', 'Send the transactions', false)
 	.option('--seed <seed>', 'Seed for mint derivation', 'bank-launch')
-	.option('--no-streamflow', 'Use unlocked buckets instead of streamflow')
+	.option('--streamflow', 'Use Streamflow buckets', true)
+	.option('--no-streamflow', 'Use unlocked buckets instead of Streamflow buckets')
 	.option('--start-step <number>', 'Step to start from (0-indexed)', '0')
 	.action(async (options) => {
 		const logger = globalLogger.getSubLogger({ name: 'transition' });
